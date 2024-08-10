a86e5d0c9e7819ba7d21381b413ad3d429b9a5c6
ttasnim5 <tahsintasnim6@gmail.com>
2024-08-10 02:13:34 -0400

    add resume information in + add a work history section

--- my_portfolio/src/components/SkillsandTech.jsx
+++ my_portfolio/src/components/SkillsandTech.jsx
@@ -1,24 +1,19 @@
 import React from 'react'
 
-// 5. Highlight stack skills:
-//    - Showcase projects that demonstrate both frontend and backend capabilities
-//    - Include a variety of technologies (e.g., React, Node.js, Python, databases)
-
-// 6. Feature game development projects:
-//    - Include screenshots, GIFs, or video demos of  games
-//    - Explain the technologies or engines used (e.g., Unity, Unreal, Godot)
-//    - If possible, provide playable demos
-
-
 function SkillsandTech() {
   return (
     <div>
         <h1>Skills and Competencies</h1>
         <p>
-            Technical skills (programming languages, tools, frameworks)
-            Soft skills (communication, teamwork, problem-solving)
+            I have experience in JAVA, JavaScript, Python, and SQL. 
+            More specifically, I have experience utilizing the PERN Stack 
+            (PostgreSQL, ExpressJS, React-Native, and NodeJS).
         </p>
-
+        <p>
+            I have also amassed skills in Project Organization and Presentation, Microsoft Excel/Word/Powerpoint,
+            Adobe Photoshop, and ___. I have much experience working with team members and colleagues.
+            I am also fluent in Bengali and conversational in Spanish.
+        </p>
     </div>
   )
 }
---
+++ my_portfolio/src/components/WorkHistory.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+function WorkHistory() {
+  return (
+    <div>
+        <h1>Work Experiences</h1>
+        <p>
+            Talk abt Mike internship
+        </p>
+
+        <p>
+            Fitness Factory, Yonkers, NY, Front Desk Representative                
+                 Greeted current and prospective clients and responded to inquiries, requests, and concerns in a timely and friendly manner
+                 Processed, verified, and updated member payments and credit card data 
+                 Presented tours and information about gym equipment, amenities, personal training, and membership options
+                 Oversaw the upkeep and maintenance of the gym environment
+        </p>
+
+        <p>
+            Yonkers Partners in Education, Yonkers, NY, Summer Programs Facilitator
+             Directed the Regeneron STEM Program
+                o Advised and improved research plans for over 45 research students
+                o Connected research students with qualified mentors in different fields
+             Co-facilitated the Yonkers-Zone city-wide research project
+                o Co-managed 20 student volunteers and coordinated weekly agendas for the six-week project
+                o Collaborated with multiple partner organizations to finalize outreach plans
+        </p>
+
+        <p>
+            Fenway Dining, Boston University, Boston, MA, Service Representative           
+             Arranged scaled recipes and weekly meal plan reports on a 5 week cycle
+             Assisted management with daily post-production analysis and inventory statistics
+             Communicated clearly and efficiently with colleagues in a high-volume, fast-paced environment
+        </p>
+
+        <p>
+            Charles E. Shain Library, Linda Lear Center, New London, CT, Student Archivist
+             Organized, processed, and digitized historical collections 
+             Assisted researchers in finding and analyzing relevant works
+        </p>
+    </div>
+  )
+}
+
+export default WorkHistory
