16af418799408f4414d5d1a2e74ca7d1ac3ddc2b
ttasnim5 <tahsintasnim6@gmail.com>
2024-08-09 20:12:09 -0400

    skeleton of site created

---
+++ my_portfolio/src/components/SkillsandTech.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+// 5. Highlight stack skills:
+//    - Showcase projects that demonstrate both frontend and backend capabilities
+//    - Include a variety of technologies (e.g., React, Node.js, Python, databases)
+
+// 6. Feature game development projects:
+//    - Include screenshots, GIFs, or video demos of  games
+//    - Explain the technologies or engines used (e.g., Unity, Unreal, Godot)
+//    - If possible, provide playable demos
+
+
+function SkillsandTech() {
+  return (
+    <div>SkillsandTech</div>
+  )
+}
+
+export default SkillsandTech
