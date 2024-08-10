3b05f2f24bf2746dae47af93fd7ab5259de6041a
ttasnim5 <tahsintasnim6@gmail.com>
2024-08-09 20:39:35 -0400

    further built skeleton with prompts and minor styling, need to actually add the substance and navigation

--- my_portfolio/src/components/ContactInfo.jsx
+++ my_portfolio/src/components/ContactInfo.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function ContactInfo() {
   return (
-    <div>ContactInfo</div>
+    <div>
+        <h1>Contact Information</h1>
+        <p>Please email me at tahsintasnim6@gmail.com if you would like to get in contact!</p>
+    </div>
   )
 }
 
