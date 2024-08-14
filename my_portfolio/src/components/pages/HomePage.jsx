eb6d41c74cfbc49849241330707fb9394d5860b9
ttasnim5 <tahsintasnim6@gmail.com>
2024-08-14 02:01:39 -0400

    messed with scss a bit, tried to add videoplayer but it didnt work :4

---
+++ my_portfolio/src/components/pages/HomePage.jsx
@@ -0,0 +1,24 @@
+// src/components/HomePage.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const HomePage = () => {
+  const tiles = [
+    { name: 'About Me', path: '/AboutMe' },
+    { name: 'Projects', path: '/Projects' },
+    { name: 'Work Experiences', path: '/WorkHistory' },
+    { name: 'Skills+', path: '/SkillsandTech' },
+  ];
+
+  return (
+    <div className="home-page">
+      {tiles.map((tile, index) => (
+        <Link to={tile.path} key={index} className="tile">
+          {tile.name}
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default HomePage;
