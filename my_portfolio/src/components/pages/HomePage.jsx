2c0ba38c6d00424386ac040a557076c31a4e0938
ttasnim5 <tahsintasnim6@gmail.com>
2024-08-15 02:33:30 -0400

    added all the bg images and more styling -- may need to change navbar bg -- need to add in media properly

--- my_portfolio/src/components/pages/HomePage.jsx
+++ my_portfolio/src/components/pages/HomePage.jsx
@@ -1,21 +1,30 @@
 // src/components/HomePage.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import pink_bg from '../../assets/pink-bg.jpg'
+import blue_bg from '../../assets/blue-bg.jpg'
+import green_bg from '../../assets/green-bg.jpg'
+import orange_bg from '../../assets/orange-bg.jpg'
 
 const HomePage = () => {
   const tiles = [
-    { name: 'About Me', path: '/AboutMe' },
-    { name: 'Projects', path: '/Projects' },
-    { name: 'Work Experiences', path: '/WorkHistory' },
-    { name: 'Skills+', path: '/SkillsandTech' },
+    { name: 'About Me', path: '/AboutMe', background: pink_bg },
+    { name: 'Projects', path: '/Projects', background: blue_bg },
+    { name: 'Work Experiences', path: '/WorkHistory', background: green_bg },
+    { name: 'Skills+', path: '/SkillsandTech', background: orange_bg },
   ];
 
   return (
     <div className='home-page'>
         <div className="tiles-container">
             {tiles.map((tile, index) => (
-                <Link to={tile.path} key={index} className="tile">
-                {tile.name}
+                <Link 
+                  to={tile.path} 
+                  key={index} 
+                  className="tile" 
+                  style={{ backgroundImage: `url(${tile.background})` }}
+                >
+                <h2>{tile.name}</h2>
                 </Link>
             ))}
       </div>
