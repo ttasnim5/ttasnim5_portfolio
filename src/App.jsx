b5a20fa04163bc96b4f16cc337a0f03207caa645
ttasnim5 <tahsintasnim6@gmail.com>
2024-08-17 00:43:45 -0400

    rework homepage url

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
         <NavBar />
 
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/Home" element={<HomePage />} />
           <Route path="/AboutMe" element={<AboutMe />} />
           <Route path="/Projects" element={<Projects />} />
           <Route path="/WorkHistory" element={<WorkHistory />} />
