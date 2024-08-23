8aba6c87753c9ad85c12b1444e6456de381067bb
ttasnim5 <tahsintasnim6@gmail.com>
2024-08-23 00:50:44 -0400

    additional styling on aboutme

--- src/components/pages/AboutMe.jsx
+++ src/components/pages/AboutMe.jsx
@@ -4,50 +4,51 @@ function AboutMe() {
   return (
     <div className='aboutme'>
         <h2>A Brief Personal Introduction</h2>
-        <p>
-            Hello, my name is Tahsin! I am currently a senior Computer Science major at Boston 
-            University, set to graduate in 2025.
-        </p>
-        <p> My interests in the field primarily revolve around 
-            backend development, cybersecurity, and sustainability in technology. 
-            <ul>
-                <li>
-                    I'm drawn to backend work because I find it both challenging and rewarding, and 
-                    I've already gained some valuable experience in this area through personal and work 
-                    projects.
-                </li>
-                <li>
-                Cybersecurity fascinates me, especially given its dynamic nature and the constant 
-                innovation required to keep pace with emerging threats.
-                </li>
-                <li>
-                    As for sustainability, I'm passionate about aligning the technological world with our 
-                    environment. I believe we have abundant collective intelligence to make significant strides 
-                    in sustainable tech solutions.
-                </li>
-            </ul>  
-        </p>
-        <br></br>
-        <p>
-            In my professional journey so far, I've made a conscious effort to explore diverse industries, 
-            including food service, receptionist work, historical work, and tech (of course). This varied 
-            experience has been essential in broadening my perspective and helping me understand different 
-            types of work. A common thread throughout my experiences has been an emphasis on people and 
-            relationships, especially with teamwork, collaboration, and customer service. I really value 
-            those human connections, I think they  make work meaningful and remind us of the real-world 
-            impact of our efforts.
-        </p>
-        <p>
-            By learning from people across various sectors and engaging in different forms of interpersonal
-             work, I've aimed to both become a well-rounded individual and gain clarity on my career aspirations. 
-             This exploration has not only influenced my professional outlook but also reinforced my passion for 
-             technology and its potential to make a positive impact.
-        </p>
-        <p>
-            Some of my side hobbies include rock climbing, swimming, crochet, making simple furniture, and 
-            visiting parks :)
-        </p>
-    </div>
+            <div className='space'>
+                <p>
+                    Hello, my name is Tahsin! I am currently a senior Computer Science major at Boston 
+                    University, set to graduate in 2025. My interests in the field primarily revolve around 
+                    backend development, cybersecurity, and sustainability in technology. 
+                </p> 
+
+                <ul>
+                    <li>
+                        I'm drawn to backend work because I find it both challenging and rewarding, and 
+                        I've already gained some valuable experience in this area through personal and work 
+                        projects.
+                    </li>
+                    <li>
+                    Cybersecurity fascinates me, especially given its dynamic nature and the constant 
+                    innovation required to keep pace with emerging threats.
+                    </li>
+                    <li>
+                        As for sustainability, I'm passionate about aligning the technological world with our 
+                        environment. I believe we have abundant collective intelligence to make significant strides 
+                        in sustainable tech solutions.
+                    </li>
+                </ul>  
+
+                <p>
+                    In my professional journey so far, I've made a conscious effort to explore diverse industries, 
+                    including food service, receptionist work, historical work, and tech (of course). This varied 
+                    experience has been essential in broadening my perspective and helping me understand different 
+                    types of work. A common thread throughout my experiences has been an emphasis on people and 
+                    relationships, especially with teamwork, collaboration, and customer service. I really value 
+                    those human connections, I think they  make work meaningful and remind us of the real-world 
+                    impact of our efforts.
+                </p>
+                <p>
+                    By learning from people across various sectors and engaging in different forms of interpersonal
+                    work, I've aimed to both become a well-rounded individual and gain clarity on my career aspirations. 
+                    This exploration has not only influenced my professional outlook but also reinforced my passion for 
+                    technology and its potential to make a positive impact.
+                </p>
+                <p>
+                    Some of my side hobbies include rock climbing, swimming, crochet, making simple furniture, and 
+                    visiting parks :)
+                </p>
+            </div>
+        </div>
   )
 }
 
